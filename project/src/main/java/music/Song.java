5203f133bb13875ebe60bb726de1c16121e5bea9
quengochy <77215118+quengochy@users.noreply.github.com>
2022-07-11 16:05:25 -0700

    add artist, title, year, genre, filepath

---
+++ project/src/main/java/music/Song.java
@@ -0,0 +1,24 @@
+package music;
+
+public class Song {
+
+    public void setArtist(String string) {
+    }
+
+    public void setTitle(String string) {
+    }
+
+    public void setYear(String string) {
+    }
+
+    public void setGenre(String string) {
+    }
+
+    public void setFilePath(String string) {
+    }
+
+    public String getTitle() {
+        return null;
+    }
+
+}
